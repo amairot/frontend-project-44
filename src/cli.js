057f42383fb5b9c63fcba72fe86519971781d673
Anastasia <anagemixia@gmail.com>
2022-12-17 00:28:57 +0300

    add eslint

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export const getUserName = () => {
-    const userName = readlineSync.question('May I have your name? ');
-    return userName;
+const getUserName = () => {
+  const userName = readlineSync.question('May I have your name? ');
+  return userName;
 };
+
+export default getUserName;
