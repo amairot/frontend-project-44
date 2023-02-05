d82b6d53a2b730f4aa3605cb3ca58faf8220ea80
Anastasia <anagemixia@gmail.com>
2023-02-05 12:37:12 +0300

    modified prime game

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,22 +1,12 @@
-import generateRandomNumber from '../rng.js';
+import generateRandomNumber from '../resources/rng.js';
 import gameRun from '../index.js';
+import ifPrime from '../resources/prime-check.js';
 
 const primeGame = () => {
   const primeRule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
   const generatePrime = () => {
     const questionPrime = generateRandomNumber(20);
-    let answerPrime = '';
-    let divisionCount = 0;
-    for (let i = 1; i <= (questionPrime / 2); i += 1) {
-      if ((questionPrime % i) === 0) {
-        divisionCount += 1;
-      }
-    }
-    if (divisionCount === 1) {
-      answerPrime = 'yes';
-    } else {
-      answerPrime = 'no';
-    }
+    const answerPrime = ifPrime(questionPrime);
     const QnA = [questionPrime, `${answerPrime}`];
     return QnA;
   };
---
+++ src/resources/prime-check.js
@@ -0,0 +1,14 @@
+const ifPrime = (number) => {
+    let divisionCount = 0;
+    for (let i = 1; i <= (number / 2); i += 1) {
+      if ((number % i) === 0) {
+        divisionCount += 1;
+      }
+    }
+    if (divisionCount === 1) {
+      return 'yes';
+    } else {
+      return 'no';
+    }
+};
+export default ifPrime;
