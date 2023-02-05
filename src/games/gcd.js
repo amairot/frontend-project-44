f1bf6ba04179a9d0ac2d0352e356e6684a5ea969
Anastasia <anagemixia@gmail.com>
2023-02-05 12:03:21 +0300

    updated gcd game

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,5 +1,6 @@
-import generateRandomNumber from '../rng.js';
+import generateRandomNumber from '../resources/rng.js';
 import gameRun from '../index.js';
+import findGcd from '../resources/gcd-check.js';
 
 const gcdGame = () => {
   const gcdRule = 'Find the greatest common divisor of given numbers.';
@@ -8,16 +9,8 @@ const gcdGame = () => {
     const qOne = generateRandomNumber(20);
     const qTwo = generateRandomNumber(20);
     const questionGcd = `${qOne} ${qTwo}`;
-    const answerGcd = [qOne, qTwo];
-    let placeholder = 0;
-    // euclidean algorithm; the algorithm replaces (a, b) by (b, a mod b) repeatedly until the pair is (d, 0), where d is the greatest common divisor.
-    while (answerGcd[1] !== 0) { // for 48, 18
-      placeholder = answerGcd[0]; // ph = 48
-      answerGcd[0] = answerGcd[1]; // ph = 48, (18, 18)
-      answerGcd[1] = placeholder % answerGcd[0]; // ph = 48, (18, 48 mod 18) -> (18, 12) and repeat
-    }
-
-    const QnA = [questionGcd, `${answerGcd[0]}`];
+    const answerGcd = findGcd(qOne, qTwo);
+    const QnA = [questionGcd, `${answerGcd}`];
     return QnA;
   };
 
